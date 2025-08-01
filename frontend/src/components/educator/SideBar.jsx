eaef484d6bd172054d251115884e81b919d8b250
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-08-02 03:28:44 +0530

    setup pages and components and context

---
+++ frontend/src/components/educator/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBar = () => {
+  return (
+    <div>
+      Edu SideBar
+    </div>
+  );
+}
+
+export default SideBar;
---
+++ frontend/src/pages/educator/StudentEnrolled.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentEnrolled = () => {
+  return (
+    <div>
+      StudentEnrolled
+    </div>
+  );
+}
+
+export default StudentEnrolled;
