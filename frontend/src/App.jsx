87b4a3617cfa254d7cf941c69a40baee7367aff0
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-08-03 00:43:03 +0530

    created Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useMatch } from 'react-router-dom';
 import Home from "./pages/student/Home"
 import CourseList from "./pages/student/CourseList"
 import CourseDetail from "./pages/student/CourseDetails"
@@ -11,12 +11,14 @@ import Dashboard from "./pages/educator/Dashboard"
 import AddCourse from "./pages/educator/AddCourse"
 import MyCourses from "./pages/educator/MyCourses"
 import StudentEnrolled from "./pages/educator/StudentEnrolled"
-import Navber from './components/student/Navbar';
+import Navbar from './components/student/Navbar';
 
 const App = () => {
+  const isEducatorRoute = useMatch("/educator/*")
+
   return (
-    <div>
-      <Navber />
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CourseList />} />
--- frontend/src/components/student/Navbar.jsx
+++ frontend/src/components/student/Navbar.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import {assets} from "../../assets/assets.js"
+import { Link } from 'react-router-dom';
 
-const Navber = () => {
+const Navbar = () => {
+  const isCourseListPage = location.hostname.includes("/course-list")
+
   return (
-    <div>
-      {/* <img src={assets.logo} alt='Logo' className='w-28' /> */}
+    <div className={`flex justify-between items-center px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? "bg-white" : "bg-cyan-100/70"}`}>
+      <img src={assets.logo} alt='Logo' className='w-28' />
+      {/* for large screen size */}
+      <div className='hidden md:flex gap-5 items-center text-gray-500'>
+        <div className='flex items-center gap-5'>
+          <button>Become Educator</button>
+          | <Link to="/my-enrollments">My Enrollments</Link>
+        </div>
+        <button className='bg-blue-600 px-5 py-2 rounded-full text-white'>Create Account</button>
+      </div>
+      {/* for small screen size */}
+      <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+        <div className='flex items-center gap-3'>
+          <button>Become Educator</button>
+          | <Link to="/my-enrollments">My Enrollments</Link>
+        </div>
+        <button><img src={ assets.user_icon } alt='user_icon' /></button>
+      </div>
     </div>
   );
 }
 
-export default Navber;
+export default Navbar;
